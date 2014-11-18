726ccaf4a8804b72d5ed18f8ef0d071b979f68e7
MikeB <mikstar@live.nl>
2014-11-18 08:39:05 +0100

    basic movement

--- ProjectMap/Assets/Scripts/PlayerMovement.cs
+++ ProjectMap/Assets/Scripts/PlayerMovement.cs
@@ -3,13 +3,69 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	public float speed;
+	public string InptHor;
+	public string InptVer;
+	public string InptAimHor;
+	public string InptAimVer;
+	private GameObject aimBase;
+
+	delegate void AimTurn();
+	AimTurn aimTurn;
+
+	void Start()
+	{
+		aimBase = transform.GetChild(0).gameObject;
+
+		if(InptAimHor == "Mouse")
+		{
+			aimTurn = mouseAim;
+		}
+		else
+		{
+			aimTurn = stickAim;
+		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () 
+	{
+		//turn/move the moveBase
+		if(Input.GetAxis(InptHor) != 0 || Input.GetAxis(InptVer) != 0)
+		{
+			float rotation = Mathf.Atan2(Input.GetAxis(InptHor),Input.GetAxis(InptVer))*Mathf.Rad2Deg;
+			transform.eulerAngles = new Vector3(0,rotation,0);
+			rigidbody.AddRelativeForce(new Vector3(0,0,45*speed));
+		}
+		//turn the aimBase
+		aimTurn();
+
+
+	}
+
+	private void stickAim()
+	{
+		if(Input.GetAxis(InptHor) != 0 || Input.GetAxis(InptVer) != 0)
+		{
+			float rotation = Mathf.Atan2(Input.GetAxis(InptAimHor),Input.GetAxis(InptAimVer))*Mathf.Rad2Deg;
+			aimBase.transform.eulerAngles = new Vector3(0,rotation,0);
+		}
+	}
+
+	private void mouseAim()
+	{
+		Ray ray =  Camera.main.ScreenPointToRay(Input.mousePosition);
+		Vector3 aimPoint = Physics.RaycastAll(ray,100,(1<<8))[0].point;
+		Vector3 delta = aimPoint - transform.position;
+		aimBase.transform.eulerAngles = new Vector3(0,Mathf.Atan2(delta.x,delta.z)*Mathf.Rad2Deg,0);
 	}
 }
+
+
+
+
+
+
+
+
+
---
+++ ProjectMap/Assets/Scripts/Weapons/WeaponManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponManager : MonoBehaviour {
+
+	public string inptWepChnge;
+	private int wepIndx = 0;
+	private WeaponBase[] weapons;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
